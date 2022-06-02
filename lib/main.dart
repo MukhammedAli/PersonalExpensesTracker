9d9842dec4d407fd3ad3ebf30e7654acaa68e042
MukhammedAli <190103412@stu.sdu.edu.kz>
2022-06-02 17:34:45 +0600

    Applying basic styling of transactions

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Container(
             width: double.infinity,
@@ -55,10 +55,45 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          Card(
-            color: Colors.red,
-            child: Text('LIST OF TX'),
-            elevation: 5,
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 30,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text(tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        )),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(tx.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          )),
+                      Text(
+                        tx.date.toString(),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
+                ]),
+              );
+            }).toList(),
           ),
         ],
       ),
